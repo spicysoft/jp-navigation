44afc12770a9975e0bb28909d7b9fea32f1387ed
Nathan Dubord <ndubord@gitlab.com>
2022-04-19 14:11:42 -0400

    named exports

--- src/install.js
+++ src/install.js
@@ -1,6 +1,8 @@
 import SlpNavigation from "./components/Navigation/Navigation.vue";
 import SlpFooter from "./components/Footer/Footer.vue";
 
+export { SlpNavigation, SlpFooter };
+
 export default {
   SlpNavigation,
   SlpFooter,
