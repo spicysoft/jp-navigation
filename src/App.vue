55bbcefe55aa4939b0b04a4d0f48c4b42b9c0c31
Nathan Dubord <ndubord@gitlab.com>
2022-04-25 03:05:10 -0400

    Add more content to the app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,95 @@
 <template>
   <div id="app">
     <SlpNavigation />
-    <h1 class="app-header">Navigation repository</h1>
-    <p class="app-body">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel diam
-      sit amet augue euismod bibendum id ac leo. Pellentesque diam nisi,
-      ultrices ac lectus tempus, laoreet posuere purus. Sed vel nulla eget sem
-      auctor auctor. Donec maximus maximus sem sed semper. Suspendisse a ex quis
-      magna lobortis iaculis eu a lorem. Pellentesque porta nulla a rhoncus
-      lacinia. Mauris eleifend faucibus libero, eget dignissim leo tempor id.
-    </p>
+    <SlpContainer>
+      <h1 class="app-header">Navigation repository</h1>
+      <p class="app-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel am
+        sit amet augue euismod bibendum id ac leo. Pellentesque diam nisi,
+        ultrices ac lectus tempus, laoreet posuere purus. Sed vel nulla eget sem
+        auctor auctor. Donec maximus maximus sem sed semper. Suspendisse a ex is
+        magna lobortis iaculis eu a lorem. Pellentesque porta nulla a rhoncus
+        lacinia. Mauris eleifend faucibus libero, eget dignissim leo tempor id.
+      </p>
+      <p class="app-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel di
+        sit amet augue euismod bibendum id ac leo. Pellentesque diam nisi,
+        ultrices ac lectus tempus, laoreet posuere purus. Sed vel nulla eget sem
+        auctor auctor. Donec maximus maximus sem sed semper. Suspendisse a ex qu
+        magna lobortis iaculis eu a lorem. Pellentesque porta nulla a rhoncus
+        lacinia. Mauris eleifend faucibus libero, eget dignissim leo tempor id.
+      </p>
+      <p class="app-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel di
+        sit amet augue euismod bibendum id ac leo. Pellentesque diam nisi,
+        ultrices ac lectus tempus, laoreet posuere purus. Sed vel nulla eget sem
+        auctor auctor. Donec maximus maximus sem sed semper. Suspendisse a ex qu
+        magna lobortis iaculis eu a lorem. Pellentesque porta nulla a rhoncus
+        lacinia. Mauris eleifend faucibus libero, eget dignissim leo tempor id.
+      </p>
+      <p class="app-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel di
+        sit amet augue euismod bibendum id ac leo. Pellentesque diam nisi,
+        ultrices ac lectus tempus, laoreet posuere purus. Sed vel nulla eget sem
+        auctor auctor. Donec maximus maximus sem sed semper. Suspendisse a ex qu
+        magna lobortis iaculis eu a lorem. Pellentesque porta nulla a rhoncus
+        lacinia. Mauris eleifend faucibus libero, eget dignissim leo tempor id.
+      </p>
+      <p class="app-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel
+        diam sit amet augue euismod bibendum id ac leo. Pellentesque diam nisi,
+        ultrices ac lectus tempus, laoreet posuere purus. Sed vel nulla eget sem
+        auctor auctor. Donec maximus maximus sem sed semper. Suspendisse a ex
+        quis magna lobortis iaculis eu a lorem. Pellentesque porta nulla a
+        rhoncus lacinia. Mauris eleifend faucibus libero, eget dignissim leo
+        tempor id.
+      </p>
+      <p class="app-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel
+        diam sit amet augue euismod bibendum id ac leo. Pellentesque diam nisi,
+        ultrices ac lectus tempus, laoreet posuere purus. Sed vel nulla eget sem
+        auctor auctor. Donec maximus maximus sem sed semper. Suspendisse a ex
+        quis magna lobortis iaculis eu a lorem. Pellentesque porta nulla a
+        rhoncus lacinia. Mauris eleifend faucibus libero, eget dignissim leo
+        tempor id.
+      </p>
+      <p class="app-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel
+        diam sit amet augue euismod bibendum id ac leo. Pellentesque diam nisi,
+        ultrices ac lectus tempus, laoreet posuere purus. Sed vel nulla eget sem
+        auctor auctor. Donec maximus maximus sem sed semper. Suspendisse a ex
+        quis magna lobortis iaculis eu a lorem. Pellentesque porta nulla a
+        rhoncus lacinia. Mauris eleifend faucibus libero, eget dignissim leo
+        tempor id.
+      </p>
+      <p class="app-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel
+        diam sit amet augue euismod bibendum id ac leo. Pellentesque diam nisi,
+        ultrices ac lectus tempus, laoreet posuere purus. Sed vel nulla eget sem
+        auctor auctor. Donec maximus maximus sem sed semper. Suspendisse a ex
+        quis magna lobortis iaculis eu a lorem. Pellentesque porta nulla a
+        rhoncus lacinia. Mauris eleifend faucibus libero, eget dignissim leo
+        tempor id.
+      </p>
+      <p class="app-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel
+        diam sit amet augue euismod bibendum id ac leo. Pellentesque diam nisi,
+        ultrices ac lectus tempus, laoreet posuere purus. Sed vel nulla eget sem
+        auctor auctor. Donec maximus maximus sem sed semper. Suspendisse a ex
+        quis magna lobortis iaculis eu a lorem. Pellentesque porta nulla a
+        rhoncus lacinia. Mauris eleifend faucibus libero, eget dignissim leo
+        tempor id.
+      </p>
+      <p class="app-body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vel
+        diam sit amet augue euismod bibendum id ac leo. Pellentesque diam nisi,
+        ultrices ac lectus tempus, laoreet posuere purus. Sed vel nulla eget sem
+        auctor auctor. Donec maximus maximus sem sed semper. Suspendisse a ex
+        quis magna lobortis iaculis eu a lorem. Pellentesque porta nulla a
+        rhoncus lacinia. Mauris eleifend faucibus libero, eget dignissim leo
+        tempor id.
+      </p>
+    </SlpContainer>
     <SlpFooter />
   </div>
 </template>
@@ -18,10 +98,12 @@
 import Vue from "vue";
 import SlpFooter from "./components/Footer/Footer.vue";
 import SlpNavigation from "./components/Navigation/Navigation.vue";
+import { SlpContainer } from "slippers-ui";
 
 export default Vue.extend({
   name: "App",
   components: {
+    SlpContainer,
     SlpFooter,
     SlpNavigation,
   },
@@ -30,7 +112,7 @@ export default Vue.extend({
 
 <style lang="scss">
 .app-header {
-  margin-top: 32px;
+  margin-top: 128px;
   margin-bottom: 64px;
 }
 
